51d09422af455af7741f12cc61be92b462958bc8
Nada Hasni <nadasthing@gmail.com>
2022-01-20 15:50:06 +0700

    add route get with param [name]

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World! Welcome to express");
 });
 
+app.get("/:name", (req, res) => {
+  res.send(`Hello ${req.params.name}! Welcome to express`);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
