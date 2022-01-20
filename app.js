8312e916ca70d5e5fd07019b7bd528bead8a727d
Nada Hasni <nadasthing@gmail.com>
2022-01-20 15:56:18 +0700

    display default view using ejs templating

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World! Welcome to express");
+  res.render("index");
 });
 
 app.get("/:name", (req, res) => {
