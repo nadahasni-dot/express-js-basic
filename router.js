25006bb8cc7f694622b552f428efc620218f2484
Nada Hasni <nadasthing@gmail.com>
2022-03-11 14:15:07 +0700

    implement mongoose on express request

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { ObjectId } = require("mongodb");
 const router = express.Router();
-const connection = require("./connection");
+require("./mongoose");
+const User = require("./User");
 
 router.get("/", (req, res) => {
   res.send("Hello World");
@@ -9,10 +9,24 @@ router.get("/", (req, res) => {
 
 router.get("/users", async (req, res) => {
   try {
-    const db = connection.db("db_latihan");
-    const users = await db.collection("users").find().toArray();
+    const users = await User.find();
 
     res.send({ data: users });
+  } catch (error) {
+    res.send({ message: error.message || "Internal server error" });
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const users = await User.findOne({ _id: id });
+
+    if (users) {
+      res.send({ data: users });
+    } else {
+      res.send({ message: "User not found" });
+    }
   } catch (error) {
     res.send({ message: error.message || "Internal server error" });
   }
@@ -21,10 +35,9 @@ router.get("/users", async (req, res) => {
 router.post("/users", async (req, res) => {
   try {
     const { name, age, status } = req.body;
-    const db = connection.db("db_latihan");
-    const users = await db.collection("users").insertOne({ name, age, status });
+    const user = await User.create({ name, age, status });
 
-    res.send({ message: "berhasil ditambahkan" });
+    res.send({ message: "berhasil ditambahkan", data: user });
   } catch (error) {
     res.send({ message: error.message || "Internal server error" });
   }
@@ -35,19 +48,13 @@ router.put("/users/:id", async (req, res) => {
     const { id } = req.params;
     const { name, age, status } = req.body;
 
-    const db = connection.db("db_latihan");
-    const users = await db.collection("users").updateOne(
-      { _id: ObjectId(id) },
-      {
-        $set: {
-          name,
-          age,
-          status,
-        },
-      }
+    const user = await User.updateOne(
+      { _id: id },
+      { name, age, status },
+      { runValidators: true }
     );
 
-    res.send({ message: "berhasil diupdate" });
+    res.send({ message: "berhasil diupdate", data: user });
   } catch (error) {
     res.send({ message: error.message || "Internal server error" });
   }
@@ -57,10 +64,9 @@ router.delete("/users/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
-    const db = connection.db("db_latihan");
-    const users = await db.collection("users").deleteOne({ _id: ObjectId(id) });
+    const user = await User.deleteOne({ _id: id });
 
-    res.send({ message: "berhasil dihapus" });
+    res.send({ message: "berhasil dihapus", data: user });
   } catch (error) {
     res.send({ message: error.message || "Internal server error" });
   }
